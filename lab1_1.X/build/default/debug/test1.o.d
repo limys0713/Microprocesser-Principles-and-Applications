3a14821bb78e041753f79dd88e20905a92c22ae3
limys <118660392+limys0713@users.noreply.github.com>
2024-10-14 22:14:12 +0800

    Add files via upload

---
+++ lab1_1.X/build/default/debug/test1.o.d
@@ -0,0 +1,3 @@
+build/default/debug/test1.o:  \
+	C:/Users/User/MPLABXProjects/lab1_1.X/test1.asm  \
+
---
+++ lab1_1.X/build/default/debug/test2.o.d
@@ -0,0 +1,3 @@
+build/default/debug/test2.o:  \
+	C:/Users/User/MPLABXProjects/lab1_1.X/test2.asm  \
+
